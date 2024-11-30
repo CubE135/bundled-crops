66d619100ed76c6d33c474b0f6db1ef4f8f4ea19
CubE135 <github@cub3mail.de>
2024-11-30 20:25:51 +0100

    Initial commit

---
+++ src/main/java/net/cube135/bundledcrops/BundledCrops.java
@@ -0,0 +1,16 @@
+package net.cube135.bundledcrops;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BundledCrops implements ModInitializer {
+	public static final String MOD_ID = "bundled-crops";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/cube135/bundledcrops/BundledCropsClient.java
@@ -0,0 +1,10 @@
+package net.cube135.bundledcrops;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BundledCropsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
